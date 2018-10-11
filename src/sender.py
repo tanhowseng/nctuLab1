3c7325e9c91ce46104616edd85513bcd0d10b623
tanhowseng <howseng_tan@mymail.sutd.edu.sg>
2018-10-11 10:23:49 +0000

    Commit lab1 in class

--- src/sender.py
+++ src/sender.py
@@ -7,9 +7,13 @@ from scapy.all import *
 from Protocol import Protocol
 
 # Set source and destination IP address (Task 3.)
+src_ip = '10.0.1.1' 
+dst_ip = '10.0.1.2'
 
 
 # Set source and destination port (Task 3.)
+src_port = 1024 
+dst_port = 80
 
 
 
@@ -18,12 +22,12 @@ Main function
 '''
 def main():
     # Define IP header (Task 3.)
-    ip = IP()
+    ip = IP(src = src_ip, dst = dst_ip)
 
     # Define customized header (Task 3.)
-    my_id = ''
-    my_dept = ''
-    my_gender = ''
+    my_id = '0745075'
+    my_dept = 'CN'
+    my_gender = 'male'
     student = Protocol(id = my_id, dept = my_dept, gender = my_gender)
 
     # Read file and store into list
@@ -46,12 +50,25 @@ def main():
         tcp_syn_ack.show()
 
         # TCP connection - ACK (Task 3.)
+        ack = tcp_syn_ack.seq + 1       
+        tcp_ack = TCP(sport = src_port, dport = dst_port, flags = 'A', seq = 1, ack = ack)       
+        packet = ip / tcp_ack 
+        send(packet)
         print '[INFO] Send ACK'
 
         # Send packet with customized header (Task 3.)
+        ack = tcp_ack.seq + 1       
+        tcp = TCP(sport = src_port, dport = dst_port, flags = '', seq = 2, ack = ack)       
+        packet = ip / tcp / student       
+        send(packet)       
         print '[INFO] Send packet with customized header'
 
-        # Send packet with secret payload (Task 3.)
+        # Send packet with secret payload (Task 3.)q
+        ack = tcp.seq + 1 
+        tcp = TCP(sport = src_port, dport = dst_port, flags = '', seq = 3, ack = ack) 
+        payload = Raw(secret[i]) 
+        packet = ip / tcp / payload 
+        send(packet)
         print '[INFO] Send packet with secret payload'
 
         
